b2376f32f944e010fd99cf0bf0ab686d4c16d420
tranlybuu <lybuu9117@gmail.com>
2021-07-12 21:01:34 +0700

    fix

--- CTDLGT_R/Bt15_SapXepChen.R
+++ CTDLGT_R/Bt15_SapXepChen.R
@@ -1,22 +1,15 @@
-selection_sort <- function(x)
-{
-  n <- length(x)
-  for (i in 1 : (n - 1))
-  {
-    min_index <- i
-    for (j in (i + 1) : (n))
-    {
-      if (x[j] < x[min_index]) {
-        min_index = j
-      }
+insertionsort_function <- function(A){
+  for (j in 2:length(A)) {
+    key = A[j] 
+    # insert A[j] into sorted sequence A[1,...,j-1] 
+    i = j - 1 
+    while (i > 0 && A[i] > key) {
+      A[(i + 1)] = A[i]
+      i = i - 1 
     }
-    temp <- x[i]
-    x[i] <- x[min_index]
-    x[min_index] <- temp
+    A[(i + 1)] = key
   }
-  x
-}
+  A
+} 
 
-arr <- sample(1 : 100, 10)
-sorted_arr <- selection_sort(arr)
-sorted_arr
+insertionsort_function(c(5, 2, 4, 6, 1, 3))
